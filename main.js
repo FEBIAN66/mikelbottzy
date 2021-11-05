34ef4f7785f70c884b8ddf14cc5d6299ec7a8df0
F3B14N <febiansnapy@gmail.com>
2021-11-05 14:18:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,311 @@
-const _0x2fdd83=_0x2a50;(function(_0x59ab47,_0x16c751){const _0x46ee0a=_0x2a50,_0x4b4ad1=_0x59ab47();while(!![]){try{const _0x121402=parseInt(_0x46ee0a(0xd3))/0x1+-parseInt(_0x46ee0a(0xdf))/0x2*(parseInt(_0x46ee0a(0xa9))/0x3)+parseInt(_0x46ee0a(0xdc))/0x4*(-parseInt(_0x46ee0a(0xcf))/0x5)+-parseInt(_0x46ee0a(0x9a))/0x6+-parseInt(_0x46ee0a(0xd2))/0x7*(-parseInt(_0x46ee0a(0x9d))/0x8)+-parseInt(_0x46ee0a(0xa8))/0x9*(parseInt(_0x46ee0a(0xcb))/0xa)+-parseInt(_0x46ee0a(0xbf))/0xb*(-parseInt(_0x46ee0a(0xde))/0xc);if(_0x121402===_0x16c751)break;else _0x4b4ad1['push'](_0x4b4ad1['shift']());}catch(_0x48fbf5){_0x4b4ad1['push'](_0x4b4ad1['shift']());}}}(_0x473f,0xc7b5a));function _0x2a50(_0x264bfe,_0x5460a9){const _0x473fa8=_0x473f();return _0x2a50=function(_0x2a508d,_0x393cc2){_0x2a508d=_0x2a508d-0x99;let _0x19bd0f=_0x473fa8[_0x2a508d];return _0x19bd0f;},_0x2a50(_0x264bfe,_0x5460a9);}function _0x473f(){const _0x1e2da4=['fs-extra','logo','Baterai\x20:\x20','warn','batteryLevelStr','49904NYJQAH','open','69612Efutku','328IvSatp','./message/group.js','sessionName','4710540NoqUlJ','default','base64EncodedAuthInfo','80jdJqZI','../kel.js','./kel.js','batrei','[WATCH]','3.0.0','WELCOME\x20MIKEL','File\x20is\x20updated!','CB:action,,battery','level','yellow','9onbdwp','2349pptXDY','./lib/loader','textSync','.json','[RAVEL]','MIKEL','browserDescription','stringify','writeFileSync','./src/json/media.json','true','existsSync','false','live','push','Standard','./message/group','white','media','group-participants-update','Scan\x20Code\x20Qr\x20Kontol\x20Atau\x20Bot\x20Ga\x20on','cyan','5071KRSLju','./lib/functions.js','@adiwajshing/baileys','chat-update','parse','logger','readFileSync','Firefox','value','[RAVEL]','\x1aMenghubungkan\x20Kalo\x20Tidak\x20Bisa\x20Mungkin\x20Sinyal....','MIKEL','11244230cgIPWF','red','connecting','log','445auwsIY','connect','unknown','194131awNYoT','1014708WEzXAn','functions','../message/group.js','Owner\x20is\x20online\x20now!'];_0x473f=function(){return _0x1e2da4;};return _0x473f();}const {WAConnection,Browsers}=require(_0x2fdd83(0xc1)),{Functions}=require(_0x2fdd83(0xc0)),{color,bgcolor}=require('./lib/color'),fs=require(_0x2fdd83(0xd7)),figlet=require('figlet'),{uncache,nocache}=require(_0x2fdd83(0xaa)),setting=JSON[_0x2fdd83(0xc3)](fs['readFileSync']('./setting.json')),welcome=require(_0x2fdd83(0xb9));baterai=_0x2fdd83(0xd1),charging=_0x2fdd83(0xd1),global[_0x2fdd83(0xbb)]=require(_0x2fdd83(0xb2)),global[_0x2fdd83(0xd4)]=new Functions(),global[_0x2fdd83(0xd8)]={'buffer':functions['fs'][_0x2fdd83(0xc5)]('./src/images/logo.jpg'),'message':media['logo']},require(_0x2fdd83(0x9f)),nocache(_0x2fdd83(0x9e),_0x11a4f0=>console[_0x2fdd83(0xce)](color(_0x2fdd83(0xa1),_0x2fdd83(0xa7)),color('\x27'+_0x11a4f0+'\x27','cyan'),_0x2fdd83(0xa4))),require(_0x2fdd83(0xe0)),nocache(_0x2fdd83(0xd5),_0x56006d=>console[_0x2fdd83(0xce)](color(_0x2fdd83(0xa1),_0x2fdd83(0xa7)),color('\x27'+_0x56006d+'\x27',_0x2fdd83(0xa7)),_0x2fdd83(0xa4)));const starts=async(_0x75d1fa=new WAConnection())=>{const _0x1e5b9a=_0x2fdd83;_0x75d1fa[_0x1e5b9a(0xc4)][_0x1e5b9a(0xa6)]=_0x1e5b9a(0xda),console[_0x1e5b9a(0xce)](color(figlet[_0x1e5b9a(0xab)](_0x1e5b9a(0xca),{'font':_0x1e5b9a(0xb8),'horizontalLayout':_0x1e5b9a(0x9b),'vertivalLayout':_0x1e5b9a(0x9b),'width':0x50,'whitespaceBreak':![]}),_0x1e5b9a(0xbe))),console[_0x1e5b9a(0xce)](color(_0x1e5b9a(0xad),_0x1e5b9a(0xbe)),color(_0x1e5b9a(0xd6),'yellow')),console[_0x1e5b9a(0xce)](color(_0x1e5b9a(0xad),_0x1e5b9a(0xbe)),color(_0x1e5b9a(0xa3),_0x1e5b9a(0xa7))),_0x75d1fa[_0x1e5b9a(0xaf)]=[_0x1e5b9a(0xae),_0x1e5b9a(0xc6),_0x1e5b9a(0xa2)],_0x75d1fa['on']('qr',()=>{const _0x353a61=_0x1e5b9a;console[_0x353a61(0xce)](color('[',_0x353a61(0xba)),color('!',_0x353a61(0xcc)),color(']','white'),color(_0x353a61(0xbd)));}),fs[_0x1e5b9a(0xb4)]('./'+setting['sessionName']+_0x1e5b9a(0xac))&&_0x75d1fa['loadAuthInfo']('./'+setting[_0x1e5b9a(0x99)]+_0x1e5b9a(0xac)),_0x75d1fa['on'](_0x1e5b9a(0xcd),()=>{const _0x14bc8f=_0x1e5b9a;console[_0x14bc8f(0xce)](color(_0x14bc8f(0xc8),_0x14bc8f(0xbe)),color(_0x14bc8f(0xc9)));}),_0x75d1fa['on'](_0x1e5b9a(0xdd),()=>{const _0x831f84=_0x1e5b9a;console[_0x831f84(0xce)](color(_0x831f84(0xc8),'cyan'),color('Okeh\x20Bot\x20Sudah\x20On\x20Mikel\x20Ganz\x20:)'));}),await _0x75d1fa[_0x1e5b9a(0xd0)]({'timeoutMs':0x1e*0x3e8}),fs[_0x1e5b9a(0xb1)]('./'+setting[_0x1e5b9a(0x99)]+_0x1e5b9a(0xac),JSON[_0x1e5b9a(0xb0)](_0x75d1fa[_0x1e5b9a(0x9c)](),null,'\x09')),_0x75d1fa['on'](_0x1e5b9a(0xa5),_0x1ee516=>{const _0x198f5e=_0x1e5b9a;global[_0x198f5e(0xdb)]=_0x1ee516[0x2][0x0][0x1][_0x198f5e(0xc7)],global['batterylevel']=parseInt(batteryLevelStr),baterai=batterylevel;if(_0x1ee516[0x2][0x0][0x1][_0x198f5e(0xb6)]==_0x198f5e(0xb3))charging=!![];if(_0x1ee516[0x2][0x0][0x1][_0x198f5e(0xb6)]==_0x198f5e(0xb5))charging=![];console[_0x198f5e(0xce)](_0x1ee516[0x2][0x0][0x1]),console[_0x198f5e(0xce)](_0x198f5e(0xd9)+batterylevel+'%');}),global[_0x1e5b9a(0xa0)]=global[_0x1e5b9a(0xa0)]?global['batrei']:[],_0x75d1fa['on']('CB:action,,battery',_0x3afd1d=>{const _0x1978ec=_0x1e5b9a,_0x56ce85=_0x3afd1d[0x2][0x0][0x1]['value'],_0x2dff29=parseInt(_0x56ce85);global[_0x1978ec(0xa0)][_0x1978ec(0xb7)](_0x2dff29);}),_0x75d1fa['on'](_0x1e5b9a(0xbc),async _0x46805b=>{await welcome(_0x75d1fa,_0x46805b);}),_0x75d1fa['on'](_0x1e5b9a(0xc2),async _0x26a257=>{const _0x1f774a=_0x1e5b9a;require(_0x1f774a(0x9f))(_0x75d1fa,_0x26a257);});};starts();
+const
+	{
+WAConnection: _WAConnection,
+		MessageType,
+		Presence,
+		MessageOptions,
+		Mimetype,
+		WALocationMessage,
+		WA_MESSAGE_STUB_TYPES,
+		WA_DEFAULT_EPHEMERAL,
+		ReconnectMode,
+		ProxyAgent,
+		GroupSettingChange,
+		waChatKey,
+		mentionedJid,
+		processTime,
+	} = require("@adiwajshing/baileys")
+const fs = require('fs')
+const axios = require("axios")
+const figlet = require("figlet");
+const simple = require("./lib/simple.js");
+const WAConnection = simple.WAConnection(_WAConnection);
+const { forks, wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
+const { color } = require('./lib/color')
+const gcdetect = JSON.parse(fs.readFileSync('./src/gcdetect.json'))
+const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
+const colors = require('colors');
+const chalk = require('chalk')
+
+baterai = {
+    
+baterai: 0,
+    
+cas: false
+
+}
+
+require('./kel.js')
+nocache('./kel.js', module => console.log(`${module} is now updated!`))
+
+const starts = async (ALDI = new WAConnection()) => {
+    ALDI.logger.level = 'warn'
+    ALDI.version = [2, 2143, 8]
+    ALDI.browserDescription = [ 'Aldi Mengintai', '', '3.0' ]
+    console.log(banner.string)
+    ALDI.on('qr', () => {
+    ALDI.sendMessage(`62857721057781@s.whatsapp.net`, `Hai mikel`, MessageType.extendedText)
+        console.log(color('[','white'), color('!','red'), color(']','white'), color('scan the Qr code'))
+    })
+    console.log(color(figlet.textSync('                V3.0.0', {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    vertivalLayout: 'default',
+    width: 80,
+    whitespaceBreak: false
+	}), 'cyan'))
+    fs.existsSync('./mikel.json') && ALDI.loadAuthInfo('./mikel.json')
+    ALDI.on('connecting', () => {
+        start('2', 'Connecting...')
+    })
+    console.log(chalk.bgHex('#FFDF00').underline(color('HELLO IM MIkel','black')))
+     console.log(chalk.bgHex('#FFDF00').underline(color('SCRIPT WAS RUN WELL..','red')))
+    ALDI.on('open', () => {
+        success('2', 'Connected')
+    })
+    await ALDI.connect({timeoutMs: 30*1000})
+    fs.writeFileSync('./mikel.json', JSON.stringify(ALDI.base64EncodedAuthInfo(), null, '\t'))
+
+
+ALDI.on('group-participants-update', async (anu) => {
+let mdata = await ALDI.groupMetadata(anu.jid)
+const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+    try {
+    	console.log(anu)
+    mem = anu.participants[0]
+    try {
+    var pp_user = await ALDI.getProfilePicture(mem)
+    } catch (e) {
+    var pp_user = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+    }
+    if (anu.action == 'add') {
+    let mdata = await ALDI.groupMetadata(anu.jid)
+    memeg = mdata.participants.length
+    num = anu.participants[0]
+    nam = anu.jid
+    anu_user = ALDI.contacts[mem]
+    teks = `
+◧ ʜᴀʟʟᴏ
+@${num.split('@')[0]}
+□ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ
+□ ${mdata.subject}
+
+
+◧ ɪɴᴛʀᴏ ᴅᴜʟᴜ
+□ ɴᴀᴍᴀ :
+□ ᴜᴍᴜʀ :
+□ ᴋᴇʟᴀs :
+□ ᴀsᴋᴏᴛ :
+
+Deskripsi
+⌬━━━━━━━━━━━⌬
+${mdata.desc}
+⌬━━━━━━━━━━━⌬
+`
+   img = await getBuffer(pp_user)
+    const sendButImage = async(id, text1, desc1, gam1, but = [], options = {}) => {
+kma = gam1
+mhan = await ALDI.prepareMessage(mdata.id, kma, image, {thumbnail: img})
+const buttonMessages = {
+imageMessage: mhan.message.imageMessage,
+contentText: text1,
+footerText: desc1,
+buttons: but,
+headerType: 4
+}
+ALDI.sendMessage(id, buttonMessages, MessageType.buttonsMessage, options)
+}
+	sendButImage(mdata.id, teks, 'Welcome message', img,  [{buttonId: 'X - Dev Team', buttonText: {displayText: 'welcome kak👋'}, type: 1},{buttonId: 'X - Dev Team', buttonText: {displayText: 'hallo selamat datang'}, type: 1}], {thumbnail: img, "contextInfo": {mentionedJid: [num]}, quoted: {"key": {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "62895619083555-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "P", "caption": `+${num.split('@')[0]} Join`, 'jpegThumbnail': fs.readFileSync('./image/thumb.jpeg')}}}})
+      }
+//================================\\
+if (anu.action == 'remove') {
+    let mdata = await ALDI.groupMetadata(anu.jid)
+    memeg = mdata.participants.length
+    num = anu.participants[0]
+    nam = anu.jid
+    anu_user = ALDI.contacts[mem]
+    teks = `sayoooonara👋`
+    let buffa = await getBuffer(pp_user)
+    const sendButImage = async(id, text1, desc1, gam1, but = [], options = {}) => {
+kma = gam1
+mhan = await ALDI.prepareMessage(mdata.id, kma, image, {thumbnail: buffa})
+const buttonMessages = {
+imageMessage: mhan.message.imageMessage,
+contentText: text1,
+footerText: desc1,
+buttons: but,
+headerType: 4
+}
+ALDI.sendMessage(id, buttonMessages, MessageType.buttonsMessage, options)
+}
+	sendButImage(mdata.id, teks, 'Leave message', buffa, [{buttonId: 'X - Dev Team', buttonText: {displayText: 'sayooonaraa 👋'}, type: 1},{buttonId: 'X - Dev Team', buttonText: {displayText: 'beban gc kluar:v'}, type: 1}], {thumbnail: buffa, "contextInfo": {mentionedJid: [num]}, quoted: {"key": {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "62895619083555-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "P", "caption": `+${num.split('@')[0]} Leave`, 'jpegThumbnail': fs.readFileSync('./image/thumb.jpeg')}}}})
+    }
+    if (anu.action == 'promote') {
+    let mdata = await ALDI.groupMetadata(anu.jid)
+    num = anu.participants[0]
+    anu_user = ALDI.contacts[mem]
+    memeg = mdata.participants.length
+    try {
+    var pp_user = await ALDI.getProfilePicture(mem)
+    } catch (e) {
+    var pp_user = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+    }
+    vipro = `[ *PROMOTE DETECTED* ]
+
+Name : *@${num.split('@')[0]}*
+Caption : *CIE JADI ATMIN:V*
+GROUP : *${(mdata.subject)}*`
+    buffop = await getBuffer(pp_user)
+    ALDI.sendMessage(mdata.id, buffop, MessageType.image, {thumbnail: buffop,caption:vipro, contextInfo: {"mentionedJid": [num]}, quoted: {"key": {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "62895619083555-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "P", "caption": `+${num.split('@')[0]} PROMOTE ADMIN GROUP`, 'jpegThumbnail': fs.readFileSync('./image/thumb.jpeg')}}}})
+    }
+    if (anu.action == 'demote') {
+    let mdata = await ALDI.groupMetadata(anu.jid)
+    num = anu.participants[0]
+    anu_user = ALDI.contacts[mem]
+    memeg = mdata.participants.length
+    try {
+    var pp_user = await ALDI.getProfilePicture(mem)
+    } catch (e) {
+    var pp_user = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+    }
+    viproo = `[ *DEMOTE DETECTED* ]
+
+Name : *@${num.split('@')[0]}*
+Caption : *AWOKAWOK DEMOTE*
+GROUP : *${(mdata.subject)}*`
+    buffep = await getBuffer(pp_user)
+    ALDI.sendMessage(mdata.id, buffep, MessageType.image, {thumbnail: buffep, caption:viproo, contextInfo: {"mentionedJid": [num]}, quoted: {"key": {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "62895619083555-1616169743@g.us","inviteCode": "mememteeeekkeke","groupName": "P", "caption": `+${num.split('@')[0]} DEMOTE ADMIN GROUP`, 'jpegThumbnail': fs.readFileSync('./image/thumb.jpeg')}}}})
+    }
+        } catch (e) {
+    console.log('Error : %s', color(e, 'red'))
+    }
+    })
+    
+
+
+
+antidel = true
+
+ALDI.on('message-delete', async (m) => {
+
+if (m.key.remoteJid == 'status@broadcast') return
+if (!m.key.fromMe && m.key.fromMe) return
+if (antidel === false) return
+m.message = (Object.keys(m.message)[0] === 'ephemeralMessage') ? m.message.ephemeralMessage.message : m.message
+const jam = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+let d = new Date
+let locale = 'id'
+let gmt = new Date(0).getTime() - new Date('1 Januari 2021').getTime()
+let weton = ['Pahing', 'Pon','Wage','Kliwon','Legi'][Math.floor(((d * 1) + gmt) / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let calender = d.toLocaleDateString(locale, {
+day: 'numeric',
+month: 'long',
+year: 'numeric'
+})
+const type = Object.keys(m.message)[0]
+ALDI.sendMessage(m.key.remoteJid, `
+𝘼𝙉𝙏𝙄 𝘿𝙀𝙇𝙀𝙏𝙀
+
+
+*Nama  : @${m.participant.split("@")[0]}*
+    
+*Jam  : ${jam} ${week} ${calender}*
+    
+*Type  : ${type}*
+
+
+𝘼𝙉𝙏𝙄 𝘿𝙀𝙇𝙀𝙏𝙀`, MessageType.text, {quoted: m.message, contextInfo: {"mentionedJid": [m.participant]}})
+ALDI.copyNForward(m.key.remoteJid, m.message)
+})
+
+
+antical = false 
+ALDI.on("CB:Call", json => {
+if (antical === false) return
+let call;
+calling = JSON.parse(JSON.stringify(json))
+call = calling[1].from
+ALDI.sendMessage(call, `*Sorry ${ALDI.user.name} can't receive calls.*\n*Call = Block!*`, MessageType.text)
+
+.then(() => ALDI.blockUser(call, "add"))
+})
+
+
+
+
+
+
+
+
+ALDI.on('group-update', async (anu) => {
+if (!gcdetect.includes(anu.jid)) return
+fkontakk = { key: {
+                  fromMe: false,
+                  participant: `0@s.whatsapp.net`, ...(anu.jid ? { remoteJid: '6285697725326-1604595598@g.us' } : {})
+               },
+               message: {
+                  "contactMessage":{"displayName":fake,"vcard":"BEGIN:VCARD\nVERSION:3.0\nN:2;ALDI FAUZI;;;\nFN:ALDI\nitem1.TEL;waid=6285697725326:+6285697725326\nitem1.X-ABLabel:Mobile\nEND:VCARD"
+               }}}
+  metdata = await ALDI.groupMetadata(anu.jid)
+    if(anu.announce == 'false'){
+    teks = `- [ Group Opened ] -\n\n_Group telah dibuka oleh admin_\n_Sekarang semua member bisa mengirim pesan_`
+    ALDI.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(`- [ Group Opened ] - In ${metdata.subject}`)
+  }
+  else if(anu.announce == 'true'){
+    teks = `- [ Group Closed ] -\n\n_Group telah ditutup oleh admin_\n_Sekarang hanya admin yang dapat mengirim pesan_`
+    ALDI.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(`- [ Group Closed ] - In ${metdata.subject}`)
+  }
+  else if(!anu.desc == ''){
+    tag = anu.descOwner.split('@')[0] + '@s.whatsapp.net'
+    teks = `- [ Group Description Change ] -\n\nDeskripsi Group telah diubah oleh Admin @${anu.descOwner.split('@')[0]}\n• Deskripsi Baru : ${anu.desc}`
+    ALDI.sendMessage(metdata.id, teks, MessageType.text, {contextInfo: {"mentionedJid": [tag]}, quoted: fkontakk})
+    console.log(`- [ Group Description Change ] - In ${metdata.subject}`)
+  }
+  else if(anu.restrict == 'false'){
+    teks = `- [ Group Setting Change ] -\n\nEdit Group info telah dibuka untuk member\nSekarang semua member dapat mengedit info Group Ini`
+    ALDI.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(`- [ Group Setting Change ] - In ${metdata.subject}`)
+  }
+  else if(anu.restrict == 'true'){
+    teks = `- [ Group Setting Change ] -\n\nEdit Group info telah ditutup untuk member\nSekarang hanya admin group yang dapat mengedit info Group Ini`
+    ALDI.sendMessage(metdata.id, teks, MessageType.text, {quoted: fkontakk})
+    console.log(`- [ Group Setting Change ] - In ${metdata.subject}`)
+  }
+})
+    ALDI.on('chat-update', async (message) => {
+        require('./kel.js')(ALDI, message)
+    })
+}
+
+/**
+ * Uncache if there is file change
+ * @param {string} module Module name or path
+ * @param {function} cb <optional> 
+ */
+function nocache(module, cb = () => { }) {
+    console.log('Module', `'${module}'`, 'is now being watched for changes')
+    fs.watchFile(require.resolve(module), async () => {
+        await uncache(require.resolve(module))
+        cb(module)
+    })
+}
+
+/**
+ * Uncache a module
+ * @param {string} module Module name or path
+ */
+function uncache(module = '.') {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(module)]
+            resolve()
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+starts()
